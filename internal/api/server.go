d109bd7be19463fd6213c65d6fa491ddd7e60d6d
Jamal Kaksouri <jamal.kaksouri@gmail.com>
2023-02-06 23:17:39 +0330

    add api CRUD

---
+++ internal/api/server.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/jamalkaskouri/simple_bank/internal/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.PUT("/accounts", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	server.router = router
+	return server
+}
